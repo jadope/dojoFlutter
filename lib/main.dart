91d465d1115d3774ecf5c344001e9a3473486a00
= <=>
2020-09-19 11:58:13 -0500

    principal screen only

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:dojo_flutter/pages/principal_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:dojo_flutter/constants/constants.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: primary,
+          scaffoldBackgroundColor: backgroundColor,
+          textTheme: TextTheme(
+              headline4:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              headline5:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.normal),
+              button: TextStyle(color: primary)),
+          inputDecorationTheme: InputDecorationTheme(
+              enabledBorder: UnderlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Colors.white.withOpacity(0.2))))),
+      home: PrincipalScreen(),
+    );
+  }
+}
---
+++ lib/pages/principal_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PrincipalScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/perosn.jpeg'), fit: BoxFit.cover),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
